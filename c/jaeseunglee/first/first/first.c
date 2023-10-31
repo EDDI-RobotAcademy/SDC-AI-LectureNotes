df0c5c69db78066fea6ab3849c6b2da9ba90dd63
LoHenP <qwpoal119@naver.com>
2023-10-31 17:46:52 +0900

    feat: [이재승] 두 번째 C 코드 프로그래밍입니다.

--- c/jaeseunglee/first/first/first.c
+++ c/jaeseunglee/first/first/first.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
-int main(void) {
-	printf("Hello First C Programming!");
+#include "second.h"
+#include "fourth.h"
+int main(void) 
+{
+	
+	printf("Hello First C Programming!\n");
+	second_function();
+	fourth_function(7);
 	return 0;
+
+	//fourth_function에서 숫자7을 출력하도록 합시다
 }
+
