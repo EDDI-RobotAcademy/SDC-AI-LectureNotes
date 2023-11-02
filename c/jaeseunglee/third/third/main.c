c0fcbd6b3c7c602b07daabbc920f3700b2d44f43
LoHenP <qwpoal119@naver.com>
2023-11-02 16:09:29 +0900

    feat: [이재승] 주사위 게임 [SDC-AI-x]

--- c/jaeseunglee/third/third/main.c
+++ c/jaeseunglee/third/third/main.c
@@ -37,13 +37,33 @@ int main(void)
 
 	printf("\n\n\n");
 
-	
-	while (loop_count < LOOP_MAX)
+	// while 문은 for문과 마찬가지로 반복문
+	// 보편적으로 for는 약간 복잡한 구조에서 많이 사용
+	// while은 단순히 반복시킬때 주로 사용
+	// 관습적인 부분이기에 반드시 그럴 필요는 없음
+
+	// -while의 동작 과정
+	//  조건 비교는 if와 동일
+	//  소괄호 내의 조건을 만족하면 반복 시행
+	//  조건을 만족하지 못하면 중단
+
+	// loop_count가 LOOP_MAX(5)보다 작다면 반복
+	// 근데 ++이 붙어있기 때문에 loop_count는 반복마다 숫자가 1씩 늘어남
+
+	// '++' '--' 에는 전위 연산자와 후위 연산자 개념이 구별됨
+	// 전위 연산자의 경우 '++loop_count' 형태
+	// 이 경우엔 조건 비교 전에 덧셈이 계산됩니다.
+	// 후위 연산자의 경우 'loop_count++'형태
+	// 이 경우엔 조건 비교 후에 덧셈이 계산됩니다.
+	while (loop_count++ < LOOP_MAX)
 	{
-		loop_count++;
+		
 		roll_dice();
+	
 		print_dice_number();
 	}
+	
+	
 
 	return 0;
 
