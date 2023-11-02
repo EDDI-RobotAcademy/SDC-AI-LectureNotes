c58f59414e9ed2ac0c7f06d13deec43705acc31c
LoHenP <qwpoal119@naver.com>
2023-11-02 12:43:52 +0900

    test

--- c/jaeseunglee/third/third/print_random.c
+++ c/jaeseunglee/third/third/print_random.c
@@ -9,7 +9,7 @@ void print_random_number(const int count)
 {
 	int loop;
 
-	const int min = 130;
+	const int min = 13;
 	const int max = 211;
 	// srand(time(NULL)) 은 무작위 랜덤을 만드려면 항상 들어가야합니다.
 	srand(time(NULL));
