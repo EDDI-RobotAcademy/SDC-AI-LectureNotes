18226fc9b57698a3817752e1dd4399afc8105325
Falcon00030 <sksk6879@naver.com>
2023-11-03 08:55:32 +0900

    feat:[장성영] 3일차 과제 [SDC-Ai-x]

---
+++ c/sungyoungjang/과제/homework3/homework3/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+
+
+int dice() {
+	return rand() % 6 + 1;
+}
+int main() {
+	srand(time(NULL));
+
+	char player1[32] = "유비";
+	char player2[32] = "조조";
+
+	int player1dice = dice();
+	printf("유비의 주사위는 %d\n", player1dice);
+
+	int player2dice = dice();
+	printf("쬬의 주사위는 %d\n", player2dice);
+
+	if (player1dice < player2dice) {
+		printf("쬬의 승리\n");
+	}
+	if (player1dice > player2dice) {
+		printf("유비의 승리\n");
+
+	}
+}
