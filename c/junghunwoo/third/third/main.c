875777eaa5e35d29701fec393c20926c20f25a6a
Amicitia00 <wjh324@naver.com>
2023-11-02 13:28:18 +0900

    feat:[우정훈]랜덤번호출력 {SDI-AI-x]

---
+++ c/junghunwoo/third/third/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "print_even.h"
+#include "print_even_advanced.h"
+#include "print_random.h"
+
+int main(void)
+{
+	const int random_number_count = 10;
+	const int start = 3;
+	const int end = 13;
+
+	//시작과 끝의 짝수 값을 출력하는 함수
+	print_even_number_from_start_to_end(start, end);
+
+	printf("조금 더 개선한 코드 \n");
+	print_even_number_from_start_to_end_advanced(start, end);
+
+	// random 숫자 10개 출력하기
+	print_random_number(random_number_count);
+
+	return 0;
+}
