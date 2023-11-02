2ab1c255310a7bc47b906f68cd9b2787b2cfabf0
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2023-11-02 10:38:26 +0900

    feat: [황영찬] 시작 ~ 끝 사이 숫자 중 짝수 출력하기 [SDC-AI-x]

---
+++ c/youngchanhwang/thrid/thrid/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "print_even.h"
+
+int main(void)
+{
+	const int start = 3;
+	const int end = 13;
+
+	print_even_number_from_start_to_end(start, end);
+
+	return 0;
+
+}
