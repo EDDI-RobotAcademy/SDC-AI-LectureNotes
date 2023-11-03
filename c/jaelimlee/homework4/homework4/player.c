c6c2f89be88654c141bf4ac3a6477ea05ab9ce34
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-11-03 17:47:38 +0900

    feat: [이재림] 4회차 과제_코드 작성중 [SDC-AI-x]

---
+++ c/jaelimlee/homework4/homework4/player.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "player.h"
+#include <string.h>
+
+char* char_dynamic_array;
+int current_char_dynamic_array_index;
+
+void create_char_dynamic_array(char* name)
+{
+	char_dynamic_array = (char*)malloc(sizeof(char) * (strlen(name) + 1));
+}
+
+void set_name_to_char_dynamic_array(char* name)
+{
+	strncpy(char_dynamic_array, name, strlen(name) + 1);
+}
+
+void print_char_dynamic_array(void)
+{
+	printf("»ç¿ëÀÚ = %s\n", char_dynamic_array);
+}
---
+++ c/jaelimlee/homework4/homework4/player.h
@@ -0,0 +1,3 @@
+void create_char_dynamic_array(char* name);
+void set_name_to_char_dynamic_array(char* name);
+void print_char_dynamic_array(void);
