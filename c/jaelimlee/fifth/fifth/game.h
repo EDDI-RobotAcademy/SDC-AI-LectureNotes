bd02fd605f097dffc0c9675166cd841195cdca6d
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-11-06 14:57:55 +0900

    feat: [이재림] 점수를 합산하여 승자를 판정합니다 [SE-10]

--- c/jaelimlee/fifth/fifth/game.h
+++ c/jaelimlee/fifth/fifth/game.h
@@ -1,4 +1,6 @@
 void play_game(void);
 void apply_dice_skill(int current_player_index);
 void buff_to_current_player(current_player_index);
-void dbuff_to_target_player(int target_player_index);
+void debuff_to_target_player(int target_player_index);
+void player_death(int current_player_index);
+void check_winner(void);
