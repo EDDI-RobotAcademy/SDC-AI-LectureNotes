7a2fc665888b7c7d9d1867926ff0b8551aee0b1e
tkdrms420 <149447077+tkdrms420@users.noreply.github.com>
2023-11-08 14:42:03 +0900

    feat:[윤상근] seventh [sdc-x]

---
+++ c/sanggunyoun/seventh/led/entity/led.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include "led.h"
+
+int led_array_count;
+
+led *init_led_object(int count)
+{
+    const led *tmp_led_array = (led *)malloc(sizeof(led) * count);
+    led_array_count = count;
+    return tmp_led_array;
+}
+
+void set_led_status(led *array, int led_index, bool on_off)
+{
+    array[led_index].status = on_off;
+}
+
+void print_led_array_status(led *array)
+{
+    int current_led_index;
+
+    printf("led 배열 출력: \n");
+
+    for (current_led_index = 0; current_led_index < led_array_count; current_led_index++)
+    {
+        printf("%4s", array[current_led_index].status ? "on" : "off");
+    }
+    printf("\n");
+}
