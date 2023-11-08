3171885bfec47a4ac28bf36bfda3eb451f4fd890
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-11-08 15:19:14 +0900

    feat: [이재림] Cmake를 활용한 다중 파일 연동 및 Entity [SDC-AI-x]

---
+++ c/jaelimlee/seventh/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "led/entity/led.h"
+
+int main (void)
+{
+    int i;
+    led *led_array;
+    const int led_array_count = 7;
+    printf("First C Programming on Linux!\n");
+
+    led_array = init_led_object(led_array_count);
+
+    for (i = 0; i < led_array_count; i++)
+    {
+        set_led_status(led_array, i, true);
+    }
+
+    print_led_array_status(led_array);
+
+    return 0;
+}
