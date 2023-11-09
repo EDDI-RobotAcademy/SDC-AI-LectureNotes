b2831a01edb32e8460b9c69cf97199c5d6dbc23a
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2023-11-09 16:47:24 +0900

    feat: [황영찬] 구조체 포인터로 주사위게임 [SDC-AI-x]

---
+++ c/youngchanhwang/eighth/player/entity/player.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "player.h"
+
+unsigned int current_player_max;
+
+player *init_player_object(void)
+{
+    player *tmp_player = (player *)malloc(sizeof(player));
+    tmp_player->id = current_player_max++;
+
+    return tmp_player;
+}
+
+void set_player_name(player *object, char *name)
+{
+    int name_length = strlen(name) + 1;
+
+    object->name = (char *)malloc(sizeof(char) * name_length);
+    strncpy(object->name, name, name_length);
+}
+
+void print_player_object(player *object)
+{
+    printf("unique id: %d\n", object->id);
+    printf("name: %s\n", object->name);
+}
