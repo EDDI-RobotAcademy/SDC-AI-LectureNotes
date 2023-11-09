a69bfdf7d94038a18a2ce55991922268d5e27d3d
Falcon00030 <149447216+Falcon00030@users.noreply.github.com>
2023-11-09 11:38:50 +0900

    feat:[장성영]eighth(주사위추가)-1[SDC_AI_x]

---
+++ c/sungyoungjang/eighth/dice/entity/dice.c
@@ -0,0 +1,18 @@
+#include "dice.h"
+#include "../../utility/random.h"
+
+#include <stdio.h>
+
+dice *init_dice_object(void)
+{
+    dice *tmp_dice = (dice *)malloc(sizeof(dice));
+    tmp_dice->dice_number = 
+        generate_random_from_min_to_max(MIN,MAX);
+    
+    return tmp_dice;
+}
+
+void print_dice_object(dice *object)
+{
+    printf("주사위 눈금: %d\n", object->dice_number);
+}
