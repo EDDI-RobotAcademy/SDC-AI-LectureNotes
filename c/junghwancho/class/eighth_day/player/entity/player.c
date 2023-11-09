d9ae2b391a6178d0859016b1d050c2fe0cbb036e
Jungpop <shockstrut22@gmail.com>
2023-11-09 17:49:05 +0900

    feat: [조정환] 8일차 수업 마지막 업로드 [SDC-AI-x]]

--- c/junghwancho/class/eighth_day/player/entity/player.c
+++ c/junghwancho/class/eighth_day/player/entity/player.c
@@ -19,12 +19,16 @@ player *init_player_object(void)  // id 정의
 
 void set_player_name(player *object, char *name)
 // *object를 player type으로 선언하고, *name의 문자열을 받아 set_player_name 수행
+// 여기서 object라는 포인터로 player * 객체의 주소를 저장하고 아래에서 object를 이용해 player 객체 주소에 접근하도록 한다.
 {
     int name_length = strlen(name) + 1;
-    // strln
+    // name으로 받은 문자열에 + 1 을 해서 name_length에 대입
 
     object->name = (char *)malloc(name_length);
+    // name_length 만큼의 메모리를 사용하는 char * 파라메타를 생성하고 이거를 name에 대입(저장)
+    // object는 구조체 안의 정보를 가져오는데, 가독성을 위해 임의 생성한 단어로 의미는 크게 없고 구조만 알자
     strncpy(object->name, name, name_length);
+    // char *name, name_length를 받아 player 구조체의 name에 저장 
 }
 
 void print_player_object(player *object) // id, name 출력
