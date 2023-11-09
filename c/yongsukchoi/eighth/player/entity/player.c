60f5eefcfd7f6fb8a839b2b5487b5bbc1ce93797
ysjjang2779 <ysjjang2020@naver.com>
2023-11-09 18:08:29 +0900

    feat: [최용석] 주사위게임[SDC-AI-x]

--- c/yongsukchoi/eighth/player/entity/player.c
+++ c/yongsukchoi/eighth/player/entity/player.c
@@ -9,8 +9,8 @@ unsigned int current_player_max;
 player *init_player_object(void)
 {
     player *tmp_player = (player *)malloc(sizeof(player));
-    tmp_player -> id = current_player_max++;
-    
+    tmp_player->id = current_player_max++;
+
     return tmp_player;
 }
 
@@ -18,12 +18,12 @@ void set_player_name(player *object, char *name)
 {
     int name_length = strlen(name) + 1;
 
-    object -> name = (char *)malloc(sizeof(char) * name_length);
-    strncpy(object -> name, name, name_length);
+    object->name = (char *)malloc(sizeof(char) * name_length);
+    strncpy(object->name, name, name_length);
 }
 
 void print_player_object(player *object)
 {
-    printf("unique id: %d\n", object -> id);
-    printf("name: %s\n", object -> name);
+    printf("unique id: %d\n", object->id);
+    printf("name: %s\n", object->name);
 }
