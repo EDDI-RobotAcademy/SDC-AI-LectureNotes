23d58ed4b98c46757899866bb59993a13d9e1d23
Falcon00030 <149447216+Falcon00030@users.noreply.github.com>
2023-11-09 15:26:37 +0900

    feat:[장성영]eighth(주사위스킬추가)-3[SDC_AI_X]

--- c/sungyoungjang/eighth/dice/entity/dice.c
+++ c/sungyoungjang/eighth/dice/entity/dice.c
@@ -12,6 +12,12 @@ dice *init_dice_object(void)
     return tmp_dice;
 }
 
+void roll_dice(dice *object)
+{
+    object->dice_number =
+        generate_random_from_min_to_max(MIN,MAX);
+}
+
 int get_dice_number(dice *object)
 {
     return object->dice_number;
@@ -20,4 +26,19 @@ int get_dice_number(dice *object)
 void print_dice_object(dice *object)
 {
     printf("주사위 눈금: %d\n", object->dice_number);
+}
+
+void add_score(dice *object)
+{
+    object->dice_number += DICE_TWO_SKILL_NUMBER;
+}
+
+void sub_score(dice *target_object)
+{
+    target_object->dice_number += DICE_THREE_SKILL_NUMBER;
+}
+
+void death(dice *object)
+{
+    object->dice_number = DICE_DEATH_NUMBER;
 }
--- c/sungyoungjang/eighth/dice/entity/dice.h
+++ c/sungyoungjang/eighth/dice/entity/dice.h
@@ -5,14 +5,23 @@ typedef struct _dice dice;
 
 struct _dice
 {
-    unsigned int dice_number;
+    int dice_number;
 };
 
 #define MIN     1
 #define MAX     6
 
+#define DICE_TWO_SKILL_NUMBER        3
+#define DICE_THREE_SKILL_NUMBER     -4
+#define DICE_DEATH_NUMBER           -4444
+
 dice *init_dice_object(void);
 void print_dice_object(dice *object);
 int get_dice_number(dice *object);
+void add_score(dice *object);
+void sub_score(dice *target_object);
+void death(dice *object);
+void roll_dice(dice *object);
+
 
 #endif
