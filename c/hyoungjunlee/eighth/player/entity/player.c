4109e6b73dcf3404fd1e297369e6c2e19c7cbd3e
이형준 <gggudwnsgg@naver.com>
2023-11-09 16:47:08 +0900

    feat [이형준] 11/9 [sdc-ai-x]

---
+++ c/hyoungjunlee/eighth/player/entity/player.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "player.h"
+#include <string.h>
+#include <stdio.h>
+
+
+unsigned int current_player_max;
+
+player *init_player_object(void)
+{
+    player *tmp_player = (player *)malloc(sizeof(player));
+    tmp_player->id = current_player_max++;
+    return tmp_player;
+}
+
+void set_player_name(player *object, char *name)
+{
+    int name_length = strlen(name) +1;
+    object->name = (char*)malloc(name_length);
+    strncpy(object->name,name,name_length);
+}
+
+void print_player_object(player *object)
+{
+    printf("unique id: %d\n",object->id);
+    printf("name: %s\n",object->name);
+}
