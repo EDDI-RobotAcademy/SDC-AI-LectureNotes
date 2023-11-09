522c8761c2eb5f7faef5f92bd8494e2b619197d8
Jungpop <shockstrut22@gmail.com>
2023-11-09 17:10:13 +0900

    feat: [조정환] 8일차 수업 업로드 [SDC-AI-x]

--- c/junghwancho/class/eighth_day/main.c
+++ c/junghwancho/class/eighth_day/main.c
@@ -3,42 +3,220 @@
 #include "player/entity/player.h"
 #include "dice/entity/dice.h"
 #include "utility/random.h"
+#include "utility/common.h"
 
-#define MAX_PLAYER      2
-#define MAX_DICE        2
+#define MAX_PLAYER          2
+#define MAX_DICE            2
 
-int main(void)
+#define FIRST_DICE          0
+#define SECOND_DICE         1
+
+int find_target_dice(int current_index)
 {
-    int i, j;
-    player *player_object[MAX_PLAYER];
-    char *name[MAX_PLAYER] = {
-        "낵아마",
-        "낵아뭐"
-    };
-    dice *dice_array[MAX_PLAYER][MAX_DICE];
+    return current_index ? 0 : 1;
+}
 
-
-    printf("Eighth work\n");
+// 2중 배열
+// player *player_object[MAX_PLAYER];
+// 배열의 이름을 받을 수 있는 것은 포인터 * => 정보만 받아오기 위해 사용
+void player_info_allocation(player **player_object_array, char **name)
+{
+    int i;
 
     for (i = 0; i < MAX_PLAYER; i++)
     {
-        player_object[i] = init_player_object();
-        set_player_name(player_object[i], name[i]);
-        print_player_object(player_object[i]);
+        player_object_array[i] = init_player_object();
+        set_player_name(player_object_array[i], name[i]);
+        print_player_object(player_object_array[i]);
     }
+}
+
+void dice_memory_allocation(dice *dice_array[MAX_PLAYER][MAX_DICE])
+{
+    int i, j;
 
     random_seed_config();
+
+    for (i = 0; i < MAX_PLAYER; i++)
+    {
+        for (j = 0; j < MAX_DICE; j++)
+        {
+            dice_array[i][j] = init_dice_object();
+        }
+    }
+}
+
+void apply_dice_skill(bool isFirstDiceIsEven, 
+                    dice *dice_array[MAX_PLAYER][MAX_DICE], 
+                    int player_index)
+{
+    if (isFirstDiceIsEven)
+    {
+        int target_index;
+        int second_dice = get_dice_number(
+            dice_array[player_index][SECOND_DICE]);
+
+        switch (second_dice)
+        {
+            case 2:
+                printf("2번 스킬: +3\n");
+                add_score(dice_array[player_index][SECOND_DICE]);
+                break;
+            
+            case 3:
+                printf("3번 스킬: -4\n");
+                target_index = find_target_dice(player_index);
+                sub_score(dice_array[target_index][SECOND_DICE]);
+                break;
+            
+            case 4:
+                printf("뒤졌음: -4444\n");
+                death(dice_array[player_index][SECOND_DICE]);
+                break;
+            
+            default:
+                break;
+        }
+    }
+}
+
+void play_dice_game(dice *dice_array[MAX_PLAYER][MAX_DICE])
+{
+    int i, j;
 
     for (i = 0; i < MAX_PLAYER; i++)
     {
         printf("player: %d\n", i);
         for (j = 0; j < MAX_DICE; j++)
         {
-        dice_array[i][j] = init_dice_object();
-        print_dice_object(dice_array[i][j]);
-        }
-    
-    }
+            bool isFirstDiceIsEven = false;
+            if (j == SECOND_DICE)
+            {
+                // '!' 는 NOT 연산자로
+                // 참을 부정하면 거짓
+                // 거짓을 부정하면 참
+                isFirstDiceIsEven = decision_even(
+                        get_dice_number(dice_array[i][FIRST_DICE]));
 
+                if (!isFirstDiceIsEven)
+                {
+                    break;
+                }
+            }
+            
+            // 첫 번째 주사위 굴리기
+            roll_dice(dice_array[i][j]);
+            print_dice_object(dice_array[i][j]);
+
+            apply_dice_skill(isFirstDiceIsEven, dice_array, i);
+        }
+    }
+}
+
+int main (void)
+{
+    // int i, j;
+    // player 객체를 관리하는 배열
+    // player playerArray[MAX_PLAYER]; < 자바였으면
+    // player는 무엇인가 ? => 직접 custom type
+    // int 와 int *의 차이점 ? 
+    // => int는 정수 값이 들어있고 int *에는 정수 값을 저장할 수 있는 메모리의 주소 값이 들어있음
+    // int number = 4;
+    // int * = &number;
+    // number의 메모리 4
+    // pointer      number 
+    player *player_object[MAX_PLAYER];
+    char *name[MAX_PLAYER] = {
+        "낵아마",
+        "낵아뭐" 
+    };
+
+    dice *dice_array[MAX_PLAYER][MAX_DICE];
+
+    printf("Eighth work\n");
+
+    player_info_allocation(player_object, name);
+    /*
+    for (i = 0; i < MAX_PLAYER; i++)
+    {
+        player_object[i] = init_player_object();
+        set_player_name(player_object[i], name[i]);
+        print_player_object(player_object[i]);
+    }
+    */
+
+    dice_memory_allocation(dice_array);
+    /*
+    random_seed_config();
+
+    for (i = 0; i < MAX_PLAYER; i++)
+    {
+        for (j = 0; j < MAX_DICE; j++)
+        {
+            dice_array[i][j] = init_dice_object();
+        }
+    }
+    */
+
+    play_dice_game(dice_array);
+
+   /*
+    for (i = 0; i < MAX_PLAYER; i++)
+    {
+        printf("player: %d\n", i);
+        for (j = 0; j < MAX_DICE; j++)
+        {
+            bool isFirstDiceIsEven = false;
+            if (j == SECOND_DICE)
+            {
+                // '!' 는 NOT 연산자로
+                // 참을 부정하면 거짓
+                // 거짓을 부정하면 참
+                isFirstDiceIsEven = decision_even(
+                        get_dice_number(dice_array[i][FIRST_DICE]));
+
+                if (!isFirstDiceIsEven)
+                {
+                    break;
+                }
+            }
+
+            roll_dice(dice_array[i][j]);
+            print_dice_object(dice_array[i][j]);
+
+            apply_dice_skill(isFirstDiceIsEven, dice_array, i);
+
+            if (isFirstDiceIsEven)
+            {
+                int target_index;
+                int second_dice = get_dice_number(
+                    dice_array[i][SECOND_DICE]);
+
+                switch (second_dice)
+                {
+                    case 2:
+                        printf("2번 스킬: +3\n");
+                        add_score(dice_array[i][SECOND_DICE]);
+                        break;
+                    
+                    case 3:
+                        printf("3번 스킬: -4\n");
+                        target_index = find_target_dice(i);
+                        sub_score(dice_array[target_index][SECOND_DICE]);
+                        break;
+                    
+                    case 4:
+                        printf("뒤졌음: -4444\n");
+                        death(dice_array[i][SECOND_DICE]);
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    */
+    
     return 0;
 }
--- c/junghwancho/class/eighth_day/player/entity/player.c
+++ c/junghwancho/class/eighth_day/player/entity/player.c
@@ -10,16 +10,18 @@ player *init_player_object(void)  // id 정의
 {
     player *tmp_player = (player *)malloc(sizeof(player));
     // *tmp_player를 player type으로 선언
-    // player.h에 정의된 player 메모리 만큼 player *로 사용하겠다.
-    //  
+    // player.h에 정의된 player 메모리 만큼 player *로 사용하겠다. 
     tmp_player->id = current_player_max++;
-    
+    // current_player_max 값을 tmp_player의 id 값에 대입하고 ++
     return tmp_player;
+    // malloc에 의해 생성된 heap 메모리 주소에 id의 current_player_max 값 설정 후 리턴
 }
 
-void set_player_name(player *object, char *name) // name 정의
+void set_player_name(player *object, char *name)
+// *object를 player type으로 선언하고, *name의 문자열을 받아 set_player_name 수행
 {
     int name_length = strlen(name) + 1;
+    // strln
 
     object->name = (char *)malloc(name_length);
     strncpy(object->name, name, name_length);
