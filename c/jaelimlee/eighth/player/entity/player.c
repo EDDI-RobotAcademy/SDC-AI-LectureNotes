99c7a41fa09bfafd5d8de5ed8461de26f453f641
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-11-10 12:12:35 +0900

    feat: [이재림] 8회차 코드 주석 추가[SDC-AI-x]

--- c/jaelimlee/eighth/player/entity/player.c
+++ c/jaelimlee/eighth/player/entity/player.c
@@ -6,18 +6,34 @@
 
 unsigned int current_player_max;
 
+// 가장 기본적인 Virtual Memory Layout은 아래와 같음
+// Stack: 지역 변수 <- 기계어를 분석한 이유
+// Heap: 동적 할당(malloc, new)
+// Data: 전역 변수
+// Text: 기계어
+
 player *init_player_object(void)
 {
+    // Heap의 할당한 메모리가 어디에 들어가는가? tmp_player
+    // 여기서 생성된 것은? 
+    // 기능 관점: player 크기 만큼 메모리 할당
+    // 도메인 관점: player 객체 생성
     player *tmp_player = (player *)malloc(sizeof(player));
     tmp_player->id = current_player_max++;
 
+    // tmp_player를 return하면 무엇이 return 되는가? 할당된 Heap 메모리 주소
     return tmp_player;
 }
 
+// 이미 init에서 객체를 생성했으므로 object를 가지고 지지고 볶음.
 void set_player_name(player *object, char *name)
 {
     int name_length = strlen(name) + 1;
 
+    // 기능관점(너무 세부사항 관점으로 문제를 바라보고 있음): char 이름을 할당해서 object->name에 저장하기 위해
+    // 기능 관점의 추가적 확장: 포인터는 반드시 메모리를 할당하고 사용해야 함.
+    // 도메인 관점: object->name에 문자열 객체를 생성하기 위해
+    // 그래서 Java에도 String 클래스(구조체) 타입이 있는 것임.
     object->name = (char *)malloc(name_length);
     strncpy(object->name, name, name_length);
 }
