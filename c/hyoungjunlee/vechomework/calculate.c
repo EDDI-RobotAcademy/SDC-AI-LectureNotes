fc015faa0e6b027338e75f42c4e86bd8571a9f7b
이형준 <gggudwnsgg@naver.com>
2023-11-12 08:52:35 +0900

    feat [이형준] math라이브러리해결 [TE]

--- c/hyoungjunlee/vechomework/calculate.c
+++ c/hyoungjunlee/vechomework/calculate.c
@@ -21,6 +21,7 @@ void vector_length(vec* a_vec,vec* b_vec)
    cal_leng_vec->y = b_vec->y+a_vec->y;
    cal_leng_vec->z = b_vec->z+a_vec->z;
    
-   printf("왜 안됨 x더한 값 %d  y더한값 %d z더한 값 %d\n",(cal_leng_vec->x),cal_leng_vec->y,cal_leng_vec->z);
-   printf("통합 길이 => %f\n",(float)(sqrt((cal_leng_vec->x)^2+(cal_leng_vec->x)^2+(cal_leng_vec->x)^2)));
+   //printf("왜 안됨 x더한 값 %d  y더한값 %d z더한 값 %d\n",(cal_leng_vec->x),cal_leng_vec->y,cal_leng_vec->z);
+   //printf("될까? 제곱 %d  제곱 %d 제곱 %d\n",(int)pow((cal_leng_vec->x),2),(int)pow((cal_leng_vec->y),2),(int)pow((cal_leng_vec->z),2));
+   printf("통합 길이 => %.5f\n",(float)(sqrt(((int)pow((cal_leng_vec->x),2)+((int)pow((cal_leng_vec->y),2)+(int)pow((cal_leng_vec->z),2))))));
 }
