8b05e863bccf2b7047a06dd20ceca08dd19a36ca
Amicitia00 <149447590+Amicitia00@users.noreply.github.com>
2023-11-13 00:21:03 +0900

    feat:[우정훈]A, B 벡터 생성 [SE-1]

---
+++ c/junghunwoo/homework/Vector_homework/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include "vector/entity/vector.h"
+
+#define MAX_VECTOR_NAME      2
+
+void vector_name_info_allocation(vector **vector_name_object_array, char **vector_name)
+{
+    int i;
+
+    for (i = 0; i < MAX_VECTOR_NAME; i++)
+    {
+        vector_name_object_array[i] = init_vector_object();
+        set_vector_name(vector_name_object_array[i], vector_name[i]);
+        print_vector_name_object(vector_name_object_array[i]);
+    }
+}
+
+
+int main (void)
+{
+    vector *vector_name_object[MAX_VECTOR_NAME];
+    char *vector_name[MAX_VECTOR_NAME] = {
+        "AVector",
+        "BVector"
+    };
+
+    vector_name_info_allocation(vector_name_object, vector_name);
+
+
+
+    return 0;
+}
---
+++ c/junghunwoo/homework/Vector_homework/vector/entity/vector.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "vector.h"
+
+unsigned int current_vector_max;
+
+vector *init_vector_object(void)
+{
+    vector *tmp_vector = (vector *)malloc(sizeof(vector));
+    tmp_vector->vector_count  = current_vector_max++;
+
+    return tmp_vector;
+
+}
+
+void set_vector_name(vector *object, char *vector_name)
+{
+    int name_length = strlen(vector_name) + 1;
+
+    object->vector_name = (char *)malloc(name_length);
+    strncpy(object->vector_name, vector_name, name_length);
+}
+
+void print_vector_object(vector *object)
+{
+    printf("Vector_count: %d\n", object->vector_count);
+    printf("vector_name: %s\n", object->vector_name);
+}
+
