bbaa3873fd083e04e7ca12a841ce322ebbe5650c
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2023-11-13 14:20:59 +0900

    feat: [조승훈] random operation api to vector service connection[VECTOR-PROB-5]

--- c/sunghunjo/tenth/vector/adapter/in/api/random_operation/vector_random_operation_api_command_handler.c
+++ c/sunghunjo/tenth/vector/adapter/in/api/random_operation/vector_random_operation_api_command_handler.c
@@ -1,8 +1,24 @@
 #include "vector_random_operation_math_api_command_handler.h"
 
+#include "../../../../domain/model/vector_model.h"
+#include "../../../../../utility/random.h"
+
+#include "../../../../application/services/vector_service_command.h"
+#include "../../../../application/services/add/vector_add_service_command_handler.h"
+#include "../../../../application/services/sub/vector_sub_service_command_handler.h"
+#include "../../../../application/services/vector_service_command_table.h"
+
 #include <stdio.h>
 
 void vector_random_operation(void)
 {
+    int random_command = generate_random(VECTOR_ADD_SERVICE_EXECUTE, 
+                                            VECTOR_SUB_SERVICE_EXECUTE);
+                                            
+    vector_model *A_vector = init_vector_model_object();
+    vector_model *B_vector = init_vector_model_object();
+
     printf("벡터 랜덤 연산을 수행합니다!\n");
+
+    vector_service_command_table[random_command]();
 }
--- c/sunghunjo/tenth/vector/domain/model/vector_model.c
+++ c/sunghunjo/tenth/vector/domain/model/vector_model.c
@@ -1,12 +1,12 @@
-#include "vector.h"
-#include "../../utility/random.h"
+#include "vector_model.h"
+#include "../../../utility/random.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 
-vector *init_vector_object(void)
+vector_model *init_vector_model_object(void)
 {
-    vector *tmp_vector = (vector*)malloc(sizeof(vector));
+    vector_model *tmp_vector = (vector_model*)malloc(sizeof(vector_model));
     tmp_vector->x = generate_random(VECTOR_MIN,VECTOR_MAX);
     tmp_vector->y = generate_random(VECTOR_MIN,VECTOR_MAX);
     tmp_vector->z = generate_random(VECTOR_MIN,VECTOR_MAX);
@@ -14,7 +14,7 @@ vector *init_vector_object(void)
     return tmp_vector;
 }
 
-void print_vector_object(vector *object)
+void print_vector_object(vector_model *object)
 {
     printf("x :%f\n",object->x);
     printf("x :%f\n",object->y);
