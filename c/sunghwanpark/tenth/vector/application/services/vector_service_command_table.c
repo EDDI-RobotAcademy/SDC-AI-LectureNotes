738845f7668d401c76afd385ce6d1b94f13f86d3
Harry Park <harrypark960925@gmail.com>
2023-11-13 17:09:45 +0900

    feat: [박성환] vector operation api to service [VECTOR- PROB -5]

--- c/sunghwanpark/tenth/vector/application/services/vector_service_command_table.c
+++ c/sunghwanpark/tenth/vector/application/services/vector_service_command_table.c
@@ -5,13 +5,13 @@
 #include <stdio.h>
 #include "vector_service_command.h"
 
-void just_dummy(void)
+void just_dummy(vector_operation_request *nothing)
 {
-    printf("단순 더미 \n");
+    printf("단순 더미\n");
 }
 
 const vector_service_call_ptr_t \
-    vector_service_command_table[VECTOR_SERVICE_EXECUTE_BUFFER_COUNT]= {
-           [0 ... VECTOR_SERVICE_EXECUTE_BUFFER]= just_dummy, 
-           #include "vector_service_command_table_mapper.h"
-    };
+    vector_service_command_table[VECTOR_SERVICE_EXECUTE_BUFFER_COUNT] = {
+        [0 ... VECTOR_SERVICE_EXECUTE_BUFFER] = just_dummy,
+        #include "vector_service_command_table_mapper.h"
+};
