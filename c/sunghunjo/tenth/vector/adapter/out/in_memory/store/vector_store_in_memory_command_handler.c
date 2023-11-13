b389fe10541abfa3bb99f819e33dcc21fa3c1b00
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2023-11-13 16:02:06 +0900

    feat: [조승훈] Adapter Out In Memory 추가 [VECTOR-PROB-7]

---
+++ c/sunghunjo/tenth/vector/adapter/out/in_memory/store/vector_store_in_memory_command_handler.c
@@ -0,0 +1,6 @@
+#include "vector_store_in_memory_command_handler.h"
+
+void store_vector(void)
+{
+    printf("vector 결과 메모리에 저장!\n");
+}
--- c/sunghunjo/tenth/vector/application/services/add/vector_add_service_command_handler.c
+++ c/sunghunjo/tenth/vector/application/services/add/vector_add_service_command_handler.c
@@ -1,9 +1,16 @@
 #include "vector_add_service_command_handler.h"
 
+#include "../../../adapter/out/in_memory/vector_in_memory_command.h"
+#include "../../../adapter/out/in_memory/store/vector_store_in_memory_command_handler.h"
+#include "../../../adapter/out/in_memory/vector_in_memory_command_table.h"
+
 #include <stdio.h>
 
 float add_x_request_form(vector_operation_request *request_form)
 {
+    printf("뭐냐 ? first(x): %5.2f, second(x): %5.2f\n", 
+        request_form->first_vector->x, request_form->second_vector->x);
+        
     return request_form->first_vector->x + request_form->second_vector->x;
 }
 
@@ -17,7 +24,6 @@ float add_z_request_form(vector_operation_request *request_form)
     return request_form->first_vector->z + request_form->second_vector->z;
 }
 
-
 void vector_add(vector_operation_request *request_form)
 {
     vector_model vector_operation_result;
@@ -26,12 +32,12 @@ void vector_add(vector_operation_request *request_form)
     print_vector_operation_request(request_form);
 
     init_vector_model_object_with_value(
-        &vector_operation_result,
-        add_x_request_form(request_form),
-        add_y_request_form(request_form),
-        add_z_request_form(request_form)
-    );
+        &vector_operation_result, 
+        add_x_request_form(request_form), 
+        add_y_request_form(request_form), 
+        add_z_request_form(request_form));
 
     print_vector_model_object(&vector_operation_result);
-    //vector_operation_result = init_vector_model_object_with_vector();
+
+    vector_in_memory_command_table[VECTOR_IN_MEMORY_STORE]();
 }
