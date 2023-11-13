8796032dcec505d2cb68bc518f1e54d70cf70913
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-11-13 12:24:18 +0900

    feat: [이재림] Vector Math Operation API 추가 [VECTOR-PROB-4]

--- c/jaelimlee/tenth/main.c
+++ c/jaelimlee/tenth/main.c
@@ -5,9 +5,14 @@
 #include "vector/application/services/sub/vector_sub_service_command_handler.h"
 #include "vector/application/services/vector_service_command_table.h"
 
+#include "vector/adapter/in/api/vector_math_api_command.h"
+#include "vector/adapter/in/api/random_operation/vector_random_operation_math_api_command_handler.h"
+#include "vector/adapter/in/api/vector_math_api_command_table.h"
+
 int main (void)
 {
     vector_service_command_table[VECTOR_SUB_SERVICE_EXECUTE]();
+    vector_math_api_command_table[VECTOR_MATH_API_RANDOM_OPERATION]();
 
     return 0;
 }
