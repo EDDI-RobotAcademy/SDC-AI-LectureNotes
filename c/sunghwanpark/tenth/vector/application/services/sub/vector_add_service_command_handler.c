82a6e600e18559638e2422cf4a2363409422a5b9
Harry Park <harrypark960925@gmail.com>
2023-11-13 17:08:45 +0900

    feat: [박성환] vector operation api to service [VECTOR- PROB -5]

--- c/sunghwanpark/tenth/vector/application/services/sub/vector_add_service_command_handler.c
+++ c/sunghwanpark/tenth/vector/application/services/sub/vector_add_service_command_handler.c
@@ -2,7 +2,8 @@
 
 #include <stdio.h>
 
-void vector_sub(void)
+void vector_sub(vector_operation_request *request_form)
 {
-    printf("vector 뺄셈!\n");
+    printf("벡터 뺄셈입니다!\n");
+    print_vector_operation_request(request_form);
 }
