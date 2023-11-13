c7112bc6e838191d402217ada5a37d68cb51acb6
Harry Park <harrypark960925@gmail.com>
2023-11-13 17:08:58 +0900

    feat: [박성환] 계산된 결과 저장 [VECTOR- PROB -9]

--- c/sunghwanpark/tenth/vector/application/services/sub/vector_sub_service_command_handler.h
+++ c/sunghwanpark/tenth/vector/application/services/sub/vector_sub_service_command_handler.h
@@ -1,6 +1,8 @@
-#ifndef __VECTOR_SUB_SERVICE_COMMAND_H__
-#define __VECTOR_SUB_SERVICE_COMMAND_H__
+#ifndef __VECTOR_SUB_SERVICE_COMMAND_HANDLER_H__
+#define __VECTOR_SUB_SERVICE_COMMAND_HANDLER_H__
 
-void vector_sub(void);
+#include "../../../adapter/in/api/request/vector_operation_request.h"
+
+void vector_sub(vector_operation_request *);
 
 #endif
