690c9c11335eba59cd8216ed71cb55d4700a7e4a
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2023-11-14 10:29:44 +0900

    update: [박장훈] 1번 수정 및 3번 문제 답 추가 [SDC-AI-BASIC-PROB2]

--- c/janghunpark/eleventh/main.c
+++ c/janghunpark/eleventh/main.c
@@ -9,17 +9,15 @@
 // Q2. 두 개의 정수(num1, num2)를 입력받고 num1 << num2 값을 구하는 함수를 작성하고
 // 최종적으로 결과값을 출력하도록 만드시오.
 
-void print_numbers(int a, float b)
+// Q1 전용 함수
+void print_and_multiply_two_number(int a, float b)
 {
     printf("a = %d\n", a);
-    printf("b = %2f\n", b);
-}
-
-void mul_two_number(int a, float b)
-{
+    printf("b = %.2f\n", b);
     printf("a * b = %4.2f\n", a*b);
 }
 
+// Q2 전용 함수
 void check_larger_number_and_print_result(int num1, int num2)
 {
     printf("num1 = %d, num2 = %d\n", num1, num2);
@@ -38,6 +36,24 @@ void check_larger_number_and_print_result(int num1, int num2)
     }
 }
 
+// Q3 전용 함수
+void seperate_integer_and_decimals(float num)
+{
+    printf("입력받은 실수는 %.2f입니다.\n", num);
+
+    int index = MIN - 1;
+    int integral_part;
+    while ((num - index) > 1)
+    {
+        index++;
+    }
+
+    integral_part = index + 1;
+
+    printf("정수부분 = %d\n", integral_part);
+    printf("소수부분 = %.2f\n", num-integral_part);
+}
+
 int main (void)
 {
     random_config();
@@ -45,13 +61,16 @@ int main (void)
     // Q1.
     int a = generate_random_integer(MIN, MAX);
     float b = generate_random_realnumber(MIN, MAX);
-    print_numbers(a, b);
-    mul_two_number(a, b);
+    print_and_multiply_two_number(a, b);
 
     // Q2.
     int num1 = generate_random_integer(MIN, MAX);
     int num2 = generate_random_integer(MIN, MAX);
     check_larger_number_and_print_result(num1, num2);
+
+    // Q3.
+    float num = generate_random_realnumber(MIN, MAX);
+    seperate_integer_and_decimals(num);
 
     return 0;
 }
--- c/janghunpark/eleventh/utility/random/random.c
+++ c/janghunpark/eleventh/utility/random/random.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include "random.h"
 
 void random_config(void)
 {
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
 }
 
 int generate_random_integer(const int min, const int max)
@@ -14,5 +15,6 @@ int generate_random_integer(const int min, const int max)
 
 float generate_random_realnumber(const int min, const int max)
 {
-    return rand() % (max - min + 1) + min;
+    return rand() % (max - min + 1) + min + 
+        ((double)((rand() % 10000))) / 10000;
 }
