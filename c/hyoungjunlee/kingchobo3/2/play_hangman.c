b40e9f01fbfe67495263c48d22cd1c24cb2740a3
이형준 <gggudwnsgg@naver.com>
2023-11-14 16:07:39 +0900

    feat [이형준] C언어 왕초보 전용 문제 (배열) 1번만 [sdc-ai-x]

---
+++ c/hyoungjunlee/kingchobo3/2/play_hangman.c
@@ -0,0 +1,15 @@
+ #define _CRT_SECURE_NO_WARNINGS
+ #include "play_hangman.h"
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdlib.h>
+
+ 
+ void play_hangman(void)
+ {
+    char arr[10];
+    printf("입력하세요!\n");
+    scanf("%c",arr[0]);
+    printf("%c",arr[0]);
+
+ }
---
+++ c/hyoungjunlee/kingchobo3/main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "1/print_even_array.h"
+#include "2/play_hangman.h"
+
+
+int main (void)
+{   //1번
+    //print_even_array();
+
+    //2번 
+    //play_hangman();
+    //3번
+    
+
+    return 0;
+}
