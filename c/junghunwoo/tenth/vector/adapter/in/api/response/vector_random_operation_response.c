159c616ca6eb85f5d9e5c4633bfcee4109ad1fe0
Amicitia00 <wjh324@naver.com>
2023-11-14 17:50:50 +0900

    feat:[우정훈]11월 14일 문제풀기[SDI-AI-x]

--- c/junghunwoo/tenth/vector/adapter/in/api/response/vector_random_operation_response.c
+++
@@ -1,6 +0,0 @@
-#include "vector_random_operation_response.h"
-
-/*
-vector_random_operation_response *
-    init_vector_random_operation_response_object(void) 
-*/
---
+++ c/junghunwoo/test/test_function_control/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "1/function_one.h"
+#include "2/function_two.h"
+
+int main (void)
+{
+    //1번 문제
+    ski_lease(7);
+    
+    //2번 분제
+    number_dvisor(88);
+
+    return 0;
+}
