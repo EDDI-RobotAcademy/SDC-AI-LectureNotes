f1c56a36a0cd73e2c1690cae7be67ed23700e4a1
LoHenP <qwpoal119@naver.com>
2023-11-14 16:54:54 +0900

    feat :[이재승] 1114 수업 [Easy_Prob_0]

---
+++ c/jaeseunglee/quiz/test/quiz2/quiz2.h
@@ -0,0 +1,29 @@
+void quiz2_1();
+void quiz2_2();
+void quiz2_3();
+void quiz2_4();
+void quiz2_5(const int num);
+int quiz2_6(const int num1, const int num2);
+void quiz2_7();
+void quiz2_8();
+void quiz2_9();
+void quiz2_9_1(int arr[],const int size);
+int quiz2_10();
+int quiz2_10_1(int arr[], const int size);
+
+typedef struct _sample sample;
+struct _sample
+{
+    int num;
+};
+
+void quiz2_11();
+
+typedef struct _sample2 sample2;
+struct _sample2
+{
+    char *name;
+};
+
+
+void quiz2_12();
