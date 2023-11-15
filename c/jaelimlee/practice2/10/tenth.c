dda973ab7339d0e0b85400cf2e47b8debe741390
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-11-15 10:40:17 +0900

    feat: [이재림] C언어 기본 문법 숙지용 문제_내가 푼 코드

--- c/jaelimlee/practice2/10/tenth.c
+++ c/jaelimlee/practice2/10/tenth.c
@@ -1,6 +1,16 @@
 #include "tenth.h"
+#include <stdio.h>
 
 int integer_array_sum(int array[])
 {
-    return 0;
+    int sum = 0;
+
+    // 배열 크기 임의로 3 지정.
+    for (int i = 0; i < COUNT_ARRAY_2; i++)
+    {
+        sum = sum + array[i];
+
+    }
+    return sum;
+    
 }
--- c/jaelimlee/practice2/10/tenth.h
+++ c/jaelimlee/practice2/10/tenth.h
@@ -1,5 +1,6 @@
 #ifndef __TENTH_H__
 #define __TENTH_H__
+#define COUNT_ARRAY_2       3
 
 int integer_array_sum(int array[]);
 
