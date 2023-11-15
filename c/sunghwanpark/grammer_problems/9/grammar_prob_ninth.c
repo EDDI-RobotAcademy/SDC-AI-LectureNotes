f3b309fe95c57cdec460d1af2f00da4b47918c5a
Harry Park <harrypark960925@gmail.com>
2023-11-15 17:27:31 +0900

    feat: [박성환] 정수형 배열 포인터[GRAMMAR-PROB]

---
+++ c/sunghwanpark/grammer_problems/9/grammar_prob_ninth.c
@@ -0,0 +1,21 @@
+#include "grammar_prob_ninth.h"
+
+// 정수형 배열에 아무 숫자나 넣고 이 배열을 파라미터로 받아 출력하는 함수를 작성하세요.
+
+#include "../2/grammar_prob_second.h"
+
+int *alloc_array(int * count)
+{
+    int i;
+    int *tmp_array; 
+
+    *count = generate_random(4,8);
+
+    tmp_array = (int *)malloc(sizeof(int)*(*count));
+
+    for( i=0; i<*count; i++)
+    {
+        tmp_array[i] = generate_random(3,7);
+    }
+    return tmp_array;
+}
