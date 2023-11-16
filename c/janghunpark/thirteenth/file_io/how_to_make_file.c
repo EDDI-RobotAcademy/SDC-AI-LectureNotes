5b69fecd11facc62b343a3fd04eed09c8f93db32
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2023-11-16 10:41:57 +0900

    feat: [박장훈] 13회차 읽기 기능 추가 [SDC-AI-X]

--- c/janghunpark/thirteenth/file_io/how_to_make_file.c
+++ c/janghunpark/thirteenth/file_io/how_to_make_file.c
@@ -1,17 +1,21 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 #include "how_to_make_file.h"
 
-int file_open(char *filename,int option, unsigned int permission)
+int file_open(char *filename, int option, unsigned int permission)
 {
-    int fd = open(filename, option);
+    int file_descriptor = open(filename, option);
+    printf("file_descriptor = %d\n", file_descriptor);
 
-    if (fd == ERROR)
+    if (file_descriptor == ERROR)
     {
         printf("open() System Call error!\n");
         return ERROR;
     }
+
+    return file_descriptor;
 }
 
 void file_close(int file_descriptor)
