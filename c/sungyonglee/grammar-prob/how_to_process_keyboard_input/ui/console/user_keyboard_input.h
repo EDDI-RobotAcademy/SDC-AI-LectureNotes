8b50d2e507d56f8d143ffa9c598d8c434d9e3cc2
bikjo99 <149447229+bikjo99@users.noreply.github.com>
2023-11-16 16:38:03 +0900

    [이성용] 11월16일(목) 수업 작업물 [SDC-AI-x]

---
+++ c/sungyonglee/grammar-prob/how_to_process_keyboard_input/ui/console/user_keyboard_input.h
@@ -0,0 +1,9 @@
+#ifndef __USER_KEYBOARD_INPUT_H__
+#define __USER_KEYBOARD_INPUT_H__
+
+#define MAX_USER_KEYBOARD_INPUT     32
+
+void get_user_keyboard_input(char *user_keyboard_input);
+void get_user_keyboard_input_with_message(char *output_message);
+
+#endif
