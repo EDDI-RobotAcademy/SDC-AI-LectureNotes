6e8efd9744bee53afabe9569d425b977cf0ec30f
LoHenP <qwpoal119@naver.com>
2023-11-16 10:41:16 +0900

    feat: [이재승] 1116 file 작성 및 읽기 [SDC_AI_X]

---
+++ c/jaeseunglee/thirteenth/file_io/how_to_make_file.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "how_to_make_file.h"
+
+
+int file_open(char *filename, int option, unsigned int permission)
+{
+    int fd = open(filename, option, permission);
+
+    if(fd == ERROR)
+    {
+        printf("open() System Call Error!\n");
+        return ERROR;
+    }
+    return fd;
+}
+
+void file_close(int file_descriptor)
+{
+    close(file_descriptor);
+}
---
+++ c/jaeseunglee/thirteenth/file_io/how_to_read_content.h
@@ -0,0 +1,7 @@
+#ifndef __HOW_TO_READ_CONTENT_H__
+#define __HOW_TO_READ_CONTENT_H__
+
+
+void read_content_from_file(int, char* );
+void reset_file_pointer(int created_file_descriptor);
+#endif
