917af0af192f0bb2b65912b4d016dba42c489acc
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-11-16 16:34:52 +0900

    feat: [이재림] No Domain 게시물_11월 16일 수업_몰게쑤

---
+++ c/jaelimlee/thirteenth/file_io/how_to_read_content.h
@@ -0,0 +1,10 @@
+#ifndef __HOW_TO_READ_CONTENT_H__
+#define __HOW_TO_READ_CONTENT_H__
+
+#include <stdbool.h>
+
+void read_content_from_file_and_print(int, char *);
+void reset_file_pointer(int);
+void reset_file_pointer(int file_descriptor);
+
+#endif
