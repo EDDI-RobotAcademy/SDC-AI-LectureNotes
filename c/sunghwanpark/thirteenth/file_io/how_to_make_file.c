cf68d01d76d299f0e545e3657d90848c1c65b710
Harry Park <harrypark960925@gmail.com>
2023-11-16 17:03:38 +0900

    feat:[박성환] 파일에 내용 작성하기 [FILE-IO]

--- c/sunghwanpark/thirteenth/file_io/how_to_make_file.c
+++ c/sunghwanpark/thirteenth/file_io/how_to_make_file.c
@@ -1,5 +1,7 @@
 #include "how_to_make_file.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -7,11 +9,12 @@ int file_open(char *filename, int option, unsigned int permission)
 {
     int file_descriptor = open(filename, option, permission);
 
-    if(file_descriptor == ERROR)
+    if (file_descriptor == ERROR)
     {
         printf("open() System Call error!\n");
         return ERROR;
     }
+
     return file_descriptor;
 }
 
--- c/sunghwanpark/thirteenth/file_io/how_to_make_file.h
+++ c/sunghwanpark/thirteenth/file_io/how_to_make_file.h
@@ -1,7 +1,7 @@
 #ifndef __HOW_TO_MAKE_FILE_H__
 #define __HOW_TO_MAKE_FILE_H__
 
-#define ERROR (-1)
+#define ERROR               (-1)
 
 int file_open(char *filename, int option, unsigned int permission);
 void file_close(int file_descriptor);
