34e63699b880fd14174743deb2a68fd2106b23f7
Amicitia00 <wjh324@naver.com>
2023-11-17 17:25:06 +0900

    feat:[우정훈]gtest 작업[SDC-AI-x]

--- c/junghunwoo/gtest_practice/build_script.sh
+++ c/junghunwoo/gtest_practice/build_script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Print the received argument
 echo "Received argument: $TEST_RESULT"
 
-# Run the tests and capture the exit code
 $TEST_RESULT
 TEST_EXIT_CODE=$?
 
-# Print the exit code of the tests
 echo "Exit code of tests: $TEST_EXIT_CODE"
 
-# Check the exit code of the tests
+# 실제 every_unit_test 가 실행되어 테스트가 통과되면
+# c_app 을 만들면서 프로그램 컴파일이 완료됨
+# 즉 테스트가 실패하면 컴파일 또한 실패됨
 if [ $TEST_EXIT_CODE -eq 0 ]; then
-    # Build c_app if tests pass
     echo "Tests passed. Building c_app..."
     cmake --build . --target c_app
 else
