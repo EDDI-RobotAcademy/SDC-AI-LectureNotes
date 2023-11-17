2b57249c425d73aa19b7333fbb18fce71d5cce8e
Sanghoon Lee <gcccompil3r@gmail.com>
2023-11-17 16:18:57 +0900

    feat: [이상훈] Board에 GoogleTest 연동 [GTEST]

---
+++ c/lecture/board/board/domain/model/board_model.h
@@ -0,0 +1,18 @@
+#ifndef __BOARD_MODEL_H__
+#define __BOARD_MODEL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _board_model board_model;
+
+struct _board_model
+{
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
