b028e4dd57c27416933d0c8dbc2c57e4575c3bb8
Sanghoon Lee <gcccompil3r@gmail.com>
2023-11-17 14:23:13 +0900

    feat: [이상훈] Google Test 뺄셈 기능 추가하기 [GTEST]

--- c/lecture/gtest_practice/tests/add_test.cpp
+++ c/lecture/gtest_practice/tests/add_test.cpp
@@ -3,5 +3,11 @@
 
 TEST(AddTest, onePlusTwoGivesThree)
 {
-    EXPECT_EQ(5, add(1, 2));
+    EXPECT_EQ(3, add(1, 2));
+}
+
+// ? 같은 특수 문자를 적지마세요.
+TEST(덧셈_테스트, 이거_더하기_저거_되는거야)
+{
+    EXPECT_EQ(3, add(1, 2));
 }
