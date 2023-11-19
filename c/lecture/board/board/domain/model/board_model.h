60b0c1d3a8546d5cf1911136d97c4ee665713264
silenc3502 <gcccompil3r@gmail.com>
2023-11-20 00:19:28 +0900

    feat: 게시물 도메인 모델

--- c/lecture/board/board/domain/model/board_model.h
+++ c/lecture/board/board/domain/model/board_model.h
@@ -5,11 +5,20 @@
 extern "C" {
 #endif
 
+#include "board_model_id.h"
+#include "board_model_info.h"
+
 typedef struct _board_model board_model;
 
 struct _board_model
 {
+    board_model_id *board_model_id;
+    board_model_info *board_model_info;
 };
+
+board_model *init_board_model_info_with_parameter(board_model_id *, board_model_info *);
+
+void clear_board_model_info(board_model *);
 
 #ifdef __cplusplus
 }
