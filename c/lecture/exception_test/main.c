6e286e0ab694adcba7caf11ca571653a6e78a0da
Sanghoon Lee <gcccompil3r@gmail.com>
2023-11-20 11:37:37 +0900

    feat: [이상훈] Exception with goto and setjmp [SDC-AI]

---
+++ c/lecture/exception_test/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+#include "utility/random.h"
+
+int main (void)
+{
+    int i, j, k;
+    int count = 0;
+    int assume_error_random_number;
+	printf("누가 goto 쓰지말라고 했어 ?\n");
+
+    config_random_seed();
+    assume_error_random_number = generate_random(10, 25);
+    printf("에러값 = %d\n", assume_error_random_number);
+
+    // There are no error.
+    //assume_error_random_number = 77;
+
+    // Nodap code.
+    // for (i = 0; i < 3; i++)
+    // {
+    //     for (j = 0; j < 3; j++)
+    //     {
+    //         for (k = 0; k < 3; k++)
+    //         {
+    //             printf("count = %d\n", count++);
+    //             if (count == assume_error_random_number)
+    //             {
+    //                 printf("에러가 발생했습니다!\n");
+    //                 break;
+    //             }
+    //         }
+
+    //         if (count == assume_error_random_number)
+    //         {
+    //             break;
+    //         }
+    //     }
+
+    //     if (count == assume_error_random_number)
+    //     {
+    //         break;
+    //     }
+    // }
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            for (k = 0; k < 3; k++)
+            {
+                printf("count = %d\n", count++);
+                if (count == assume_error_random_number)
+                {
+                    printf("에러가 발생했습니다!\n");
+                    goto error_handler;
+                }
+            }
+        }
+    }
+
+error_handler:
+    printf("지금부터 에러를 핸들링합니다!\n");
+
+    return 0;
+}
