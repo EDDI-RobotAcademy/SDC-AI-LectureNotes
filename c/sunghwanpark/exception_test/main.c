b358f0d1b6a1a2ed00e79c6b3a6df657b2c0a9b6
Harry Park <harrypark960925@gmail.com>
2023-11-20 12:29:02 +0900

    feat: 예외 처리 예시 [SDC-AI-x]

--- c/sunghwanpark/exception_test/main.c
+++ c/sunghwanpark/exception_test/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "utility/random.h"
+#include "sample/exception.h"
 
 int main (void)
 {
@@ -43,24 +44,28 @@ int main (void)
     //     }
     // }
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            for (k = 0; k < 3; k++)
-            {
-                printf("count = %d\n", count++);
-                if (count == assume_error_random_number)
-                {
-                    printf("에러가 발생했습니다!\n");
-                    goto error_handler;
-                }
-            }
-        }
-    }
+//     for (i = 0; i < 3; i++)
+//     {
+//         for (j = 0; j < 3; j++)
+//         {
+//             for (k = 0; k < 3; k++)
+//             {
+//                 printf("count = %d\n", count++);
+//                 if (count == assume_error_random_number)
+//                 {
+//                     printf("에러가 발생했습니다!\n");
+//                     goto error_handler;
+//                 }
+//             }
+//         }
+//     }
 
-error_handler:
-    printf("지금부터 에러를 핸들링합니다!\n");
+// error_handler:
+//     printf("지금부터 에러를 핸들링합니다!\n");
+
+    exception_setup_table();
+    divide_by_zero(7, generate_random(0, 1));
+    array_index_exceed(10);
 
     return 0;
 }
