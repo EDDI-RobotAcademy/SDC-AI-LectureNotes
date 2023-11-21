3b6035b5f790f17662ebdd3967f917bdfa368444
Sanghoon Lee <gcccompil3r@gmail.com>
2023-11-21 16:36:22 +0900

    feat: [이상훈] 점진적 확장을 위한 adapter input 테스트 [BOARD]

---
+++ c/lecture/board/tests/board/adapter/input/adapter_input_test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include "../../../../board/adapter/input/board_api.h"
+#include "../../../../board/adapter/input/board_api_table.h"
+
+#include "../../../../board/adapter/input/request_form/board_api_create_request_form.h"
+
+TEST(board_adapter_input, 게시판에_게시물_작성하기)
+{
+    char title[] = "안녕";
+    char writer[] = "가즈아";
+    char content[] = "어디로 갈까 ?";
+
+    board_api_create_request_form *request_form;
+    int user_choice_command_number = BOARD_API_REGISTER;
+
+    request_form = init_board_api_create_request_form(title, writer, content);
+    board_api_table[user_choice_command_number](request_form);
+
+    clear_board_api_create_request_form(request_form);
+}
+
