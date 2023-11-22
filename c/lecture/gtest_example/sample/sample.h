b6ae36d7fd04e468916839dbb86adfdecf84851a
Sanghoon Lee <gcccompil3r@gmail.com>
2023-11-22 10:39:09 +0900

    feat: [이상훈] 구글 테스트 사용 방법론 [GTEST]

--- c/lecture/gtest_example/sample/sample.h
+++ c/lecture/gtest_example/sample/sample.h
@@ -5,7 +5,22 @@
 extern "C" {
 #endif
 
-void sample(void);
+#define MAX_USER_LENGTH         32
+#define MAX_PHONE_LENGTH        32
+
+typedef struct _for_test_struct for_test_struct;
+
+struct _for_test_struct
+{
+    char *name;
+    char *phone;
+};
+
+int add_all_to_end_from_start(int, int);
+for_test_struct *init_for_test_struct_object(char *, char *);
+char *get_for_test_struct_name(for_test_struct *);
+char *get_for_test_struct_phone(for_test_struct *);
+void clear_for_test_struct_object(for_test_struct *);
 
 #ifdef __cplusplus
 }
