085be2a28385e4c849d43feb41fd7713bf50c959
LoHenP <qwpoal119@naver.com>
2023-11-30 14:45:38 +0900

    feat: [이재승] 문제풀이 직교좌표 [cpp_test]

---
+++ c++/class_test/orthogonal/Orthogonal.h
@@ -0,0 +1,17 @@
+//
+// Created by eddi on 23. 11. 30.
+//
+
+#ifndef CLASS_TEST_ORTHOGONAL_H
+#define CLASS_TEST_ORTHOGONAL_H
+
+
+class Orthogonal {
+private:
+    const int
+public:
+
+};
+
+
+#endif //CLASS_TEST_ORTHOGONAL_H
