1f18d5f34c4968d015d7cda0ae743bd2fac55007
LoHenP <qwpoal119@naver.com>
2023-11-30 14:45:57 +0900

    feat: [이재승] 문제풀이 변위 [cpp_test]

---
+++ c++/class_test/displacement/Displacement.cpp
@@ -0,0 +1,13 @@
+//
+// Created by eddi on 23. 11. 30.
+//
+
+#include "Displacement.h"
+
+Displacement::Displacement(Vector& _start, Vector& _end) : start_vector(_start), end_vector(_end) {
+    
+}
+
+Displacement::~Displacement() {
+
+}
