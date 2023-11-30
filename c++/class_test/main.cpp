78bef980ed305ecd182577a30d4956ee04b8db11
LoHenP <qwpoal119@naver.com>
2023-11-30 11:49:55 +0900

    feat:[이재승] Player 객체 생성 [SDC-AI-CPP]

---
+++ c++/class_test/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "dice/Dice.h"
+#include "player/Player.h"
+
+int main() {
+//    if(true){
+//        Dice dice;
+//        Dice dice2;
+//    }
+
+    Dice *dice = new Dice();
+    dice->rollDice();
+
+    std::cout << "얼마 나옴?" << dice->getDiceNumber() << std::endl;
+
+    // 포인터로 객체를 받았을 경우 delete가 필요함
+    delete dice;
+
+    Dice dice1;
+    dice1.rollDice();
+    std::cout << "이것도 됨??" << dice1.getDiceNumber() << std::endl;
+
+    std::string test_name = "테스트";
+    Player *player1 = new Player(test_name, 20);
+    std::cout << "이름이뭐에요??? " << player1->getPlayerName() <<
+    "\n몇살이세요???? " << player1->getPlayerAge() << std::endl;
+
+
+    delete player1;
+    std::string myName = "이재승";
+    int myAge = 30;
+    Player player(myName, myAge);
+    std::cout << "넌 이름이뭐에요??? " << player.getPlayerName() << std::endl;
+    std::cout << "넌 몇살이세요???? " << player.getPlayerAge() << std::endl;
+
+
+
+    return 0;
+}
