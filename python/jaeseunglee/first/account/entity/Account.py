43683f3b923986397517d399f04abc331509688c
LoHenP <qwpoal119@naver.com>
2023-12-13 16:46:44 +0900

    feat: [이재승] python에서 class 사용법 익히기 [SDC-AI-python-1]

---
+++ python/jaeseunglee/first/account/entity/Account.py
@@ -0,0 +1,20 @@
+class Account:
+    def __init__(self, accountId, password):
+        self.__accountId = accountId
+        self.__password = password
+
+    def getAccountId(self):
+        return self.__accountId
+
+    def setAccountId(self, accountId):
+        self.__accountId = accountId
+
+    def getPassword(self):
+        return self.__password
+
+    def setPassword(self, password):
+        self.__password = password
+
+    # print(인스턴스) 할 때 자동으로 아래 내용을 출력함
+    def __str__(self):
+        return f"Account[{self.__accountId}:{self.__password}]"
