b12a1768cbf0a4411c72d275236c1177f39382df
tkdrms420 <149447077+tkdrms420@users.noreply.github.com>
2023-12-13 16:51:30 +0900

    feat: [윤상근] python에서 클래스 사용법 [SDC-AI-x]

--- c++/sanggunyoun/cpp_gtest/arithmetic/Add.cpp
+++ c++/sanggunyoun/cpp_gtest/arithmetic/Add.cpp
@@ -2,9 +2,11 @@
 // Created by eddi on 23. 12. 4.
 //
 
+#include <iostream>
 #include "Add.h"
 
 int Add::sum(int num1, int num2)
 {
+    std::cout << "Hello, World!" << std::endl;
     return num1 + num2;
 }
