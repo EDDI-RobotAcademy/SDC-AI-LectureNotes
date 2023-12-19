c7017613e7f01757f3fb65f7c4343cc946ee9a85
Sanghoon Lee <gcccompil3r@gmail.com>
2023-12-19 11:32:05 +0900

    feat: [이상훈] 서버용 네트워크 소켓 생성 요청 처리 [PTAN-1]

---
+++ python/lecture/second/server_socket/service/ServerSocketServiceImpl.py
@@ -0,0 +1,14 @@
+from server_socket.repository.ServerSocketRepositoryImpl import ServerSocketRepositoryImpl
+from server_socket.service.ServerSocketService import ServerSocketService
+
+
+class ServerSocketServiceImpl(ServerSocketService):
+    def __init__(self):
+        print("ServerSocketServiceImpl 생성자 호출")
+        self.__serverSocketRepository = ServerSocketRepositoryImpl()
+        # self.__clientSocketRepository = ClientSocketRepositoryImpl()
+
+    def createServerSocket(self, host, port):
+        pass
+        return self.__serverSocketRepository.create(host, port)
+
