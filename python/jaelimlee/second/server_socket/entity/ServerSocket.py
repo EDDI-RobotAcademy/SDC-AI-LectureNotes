4fc3b3e618eb05e1a0d475612f0e1ddb49103298
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-12-19 17:20:45 +0900

    feat:[이재림] 비동기 소켓 서버 생성 [PTAN]

--- python/jaelimlee/second/server_socket/entity/ServerSocket.py
+++ python/jaelimlee/second/server_socket/entity/ServerSocket.py
@@ -2,11 +2,12 @@ import atexit
 
 class ServerSocket(object):
     def __init__(self, host, port, socket):
-        self. __host = host
+        self.__host = host
         self.__port = port
         self.__socket = socket
 
         atexit.register(self.closeSocket)
+
 
     def getHost(self):
         return self.__host
@@ -16,7 +17,6 @@ class ServerSocket(object):
 
     def getSocket(self):
         return self.__socket
-
 
     def closeSocket(self):
         print("서버 소켓 종료!")
--- python/jaelimlee/second/server_socket/repository/ServerSocketRepository.py
+++ python/jaelimlee/second/server_socket/repository/ServerSocketRepository.py
@@ -5,3 +5,19 @@ class ServerSocketRepository(abc.ABC):
     @abc.abstractmethod
     def create(self, host, port):
         pass
+
+    @abc.abstractmethod
+    def setSocketOption(self, apiControlLevel, OptionName):
+        pass
+
+    @abc.abstractmethod
+    def bindServerSocket(self):
+        pass
+
+    @abc.abstractmethod
+    def setListenNumber(self, howManyAccessHere):
+        pass
+
+    @abc.abstractmethod
+    def acceptClientSocket(self):
+        pass
