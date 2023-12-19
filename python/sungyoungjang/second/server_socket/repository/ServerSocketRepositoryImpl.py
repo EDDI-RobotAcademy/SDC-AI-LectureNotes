cccd91e69bbca60eeb92afbbd07077cf37cbc103
Falcon00030 <149447216+Falcon00030@users.noreply.github.com>
2023-12-19 12:27:15 +0900

    feat:[장성영]ServerSocket구성[SDC-AI-X]

---
+++ python/sungyoungjang/second/server_socket/repository/ServerSocketRepositoryImpl.py
@@ -0,0 +1,11 @@
+from server_socket.repository.ServerSocketRepository import ServerSocketRepository
+
+
+class ServerSocketRepositoryImpl(ServerSocketRepository):
+    def __init__(self):
+        self.__serverSocket = None
+
+    def create(self, host, port):
+        socketObject = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__serverSocket = ServerSocket(host, port, socketObject)
+        return self.__serverSocket
