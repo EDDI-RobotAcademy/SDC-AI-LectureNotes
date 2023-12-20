e80e2446d01ff5e0355cdabda19b47e376da2b9c
bikjo99 <149447229+bikjo99@users.noreply.github.com>
2023-12-20 12:51:40 +0900

    feat: [이성용] 12월20일(수)수업 오전 작업물 task[SDC-AI-x]

--- python/sungyonglee/second/server_socket/repository/ServerSocketRepositoryImpl.py
+++ python/sungyonglee/second/server_socket/repository/ServerSocketRepositoryImpl.py
@@ -6,7 +6,15 @@ from server_socket.repository.ServerSocketRepository import ServerSocketReposito
 
 
 class ServerSocketRepositoryImpl(ServerSocketRepository):
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
     def __init__(self):
+        print("ServerSocketRepositoryImpl 생성자 호출")
         self.__serverSocket = None
 
     def create(self, host, port):
---
+++ python/sungyonglee/thirdUI/task_manage/service/TaskManageServiceImpl.py
@@ -0,0 +1,34 @@
+from client_socket.repository.ClientSocketRepositoryImpl import ClientSocketRepositoryImpl
+from task_manage.repository.TaskManageRepositoryImpl import TaskManageRepositoryImpl
+from task_manage.service.TaskManageService import TaskManageService
+from transmitter.repository.TransmitterRepository import TransmitterRepository
+from transmitter.repository.TransmitterRepositoryImpl import TransmitterRepositoryImpl
+
+
+class TaskManageServiceImpl(TaskManageService):
+    __instance = None
+
+    def __new__(cls, repository):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance.repository = repository
+        return cls.__instance
+
+    def __init__(self, repository):
+        print("TaskManageServiceImpl 생성자 호출")
+        self.__taskManageRepository = TaskManageRepositoryImpl()
+
+    # 파이썬은 repository=None 으로 파라미터 없으면 그냥 처리합니다
+    @classmethod
+    def getInstance(cls, repository=None):
+        if cls.__instance is None:
+            cls.__instance = cls(repository)
+        return cls.__instance
+
+    def createTransmitTask(self):
+        transmitterRepository = TransmitterRepositoryImpl.getInstance()
+
+        self.__taskManageRepository.createTask(
+            target=transmitterRepository.transmitCommand,
+            args=None
+        )
