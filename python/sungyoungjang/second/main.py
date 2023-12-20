3aeb0b2b39920efa636316ddceac078e19ac5582
Falcon00030 <149447216+Falcon00030@users.noreply.github.com>
2023-12-20 11:16:55 +0900

    feat:[장성영]ThirdUI[SDC-AI-X]

--- python/sungyoungjang/second/main.py
+++ python/sungyoungjang/second/main.py
@@ -1,15 +1,46 @@
 import socket
 from time import sleep
+from decouple import config
 
+from server_socket.repository.ServerSocketRepositoryImpl import ServerSocketRepositoryImpl
 from server_socket.service.ServerSocketServiceImpl import ServerSocketServiceImpl
 from utility.IPAddressBindSupporter import IPAddressBindSupporter
+from mysql.MySQLProcess import DbProcess
+
+MYHOST = IPAddressBindSupporter.getIpAddressFromGoogle()
+
+
+def initMysqlInstance():
+    dbInstance = DbProcess(
+        host=config('HOST'),
+        user=config('DB_USER'),
+        password=config('PASSWORD'),
+        database=config('DATABASE')
+    )
+    dbInstance.connect()
+
+
+def initServerSocketDomain():
+    serverSocketRepository = ServerSocketRepositoryImpl()
+    ServerSocketServiceImpl(serverSocketRepository)
+
+
+def initEachDomain():
+    initMysqlInstance()
+
+    initServerSocketDomain()
+
 
 if __name__ == '__main__':
     print(f"ip: {IPAddressBindSupporter.getIpAddress()}")
     print(f"ip: {IPAddressBindSupporter.getLocalIPAddress()}")
+    print(f"ip: {IPAddressBindSupporter.getIpAddressFromGoogle()}")
 
-    serverSocketService = ServerSocketServiceImpl()
-    serverSocketService.createServerSocket(IPAddressBindSupporter.getLocalIPAddress(), 33333)
+    initEachDomain()
+
+    serverSocketService = ServerSocketServiceImpl.getInstance()
+
+    serverSocketService.createServerSocket(MYHOST, int(config('PORT')))
     serverSocketService.setSocketOption(socket.SOL_SOCKET, socket.SO_REUSEADDR)
     serverSocketService.bindServerSocket()
     serverSocketService.setServerListenNumber(1)
@@ -21,3 +52,9 @@ if __name__ == '__main__':
 
         except socket.error:
             sleep(0.5)
+
+
+# 보편적으로 서비스를 제공하는 입장에 놓여 있으면 Server(서버)
+# 서비스를 받는 입장에 있으면 Client(클라이언트) 라고 합니다.
+# 앞으로 만들 UI는 현재 작성한 Server에게 요청(Request)를 합니다.
+# 그럼 UI는 Server가 준 응답(Response)를 가지고 지 나름대로의 기준으로 처리를 합니다.
---
+++ python/sungyoungjang/thirdUI/client_socket/repository/ClientSocketRepository.py
@@ -0,0 +1,15 @@
+import abc
+
+
+class ClientSocketRepository(abc.ABC):
+    @abc.abstractmethod
+    def create(self, host, port):
+        pass
+
+    @abc.abstractmethod
+    def connectionToTargetHost(self):
+        pass
+
+    @abc.abstractmethod
+    def setBlockingOperation(self):
+        pass
