302a67f9e5846fac2703fc97d79ff2d33ce411fc
Sanghoon Lee <gcccompil3r@gmail.com>
2023-12-27 12:42:36 +0900

    feat: [이상훈] Product Repository 완료 [PTAN]

--- python/lecture/answerProcess/product/entity/Product.py
+++ python/lecture/answerProcess/product/entity/Product.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
+
 from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship, Mapped
 
 from account.entity.Account import Account
 
@@ -9,20 +10,29 @@ Base = declarative_base()
 
 @dataclass
 class Product(Base):
-    __tablename__ = 'products'
+    __tablename__ = 'product'
 
     __id = Column(Integer, primary_key=True, autoincrement=True, name="id")
     __name = Column(String, nullable=False, name="name")
     __price = Column(Integer, nullable=False, name="price")
     __details = Column(String, name="details")
 
-    __registeredBy = Column(Integer, ForeignKey('account.id'), nullable=False, name="registered_by")
+    # __registeredById: int = Column(Integer, ForeignKey('account.id'), nullable=False, name="registered_by")
+    # __registeredBy: Mapped[Account] = Column(Integer, ForeignKey('account.id'), nullable=False, name="registered_by")
+    # __registeredBy = Column(Integer, ForeignKey('account.id'), nullable=False, name="registered_by")
+    # account = relationship(Account, foreign_keys=[__registeredBy])
+    __registeredBy: int = Column(Integer, nullable=False, name="registered_by")
 
     def __init__(self, name: str, price: int, details: str, registeredBy: Account):
+        print(f"Product Initialization: {name}, {price}, {details}, {registeredBy}")
         self.__name = name
         self.__price = price
         self.__details = details
-        self.__registeredBy = registeredBy
+        self.__registeredBy = registeredBy.getId()
+        print(self.__registeredBy)
+
+    def getId(self):
+        return self.__id
 
     def getName(self):
         return self.__name
--- python/lecture/answerProcess/tests/product/entity/TestProductEntity.py
+++ python/lecture/answerProcess/tests/product/entity/TestProductEntity.py
@@ -17,3 +17,4 @@ class TestProduct(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
